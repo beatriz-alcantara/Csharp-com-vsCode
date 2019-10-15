9bf606727c4f1bb641536148e9642d04adf1abc3
Beatriz813 <beatrizalcantara813@gmail.com>
2019-10-14 22:29:57 -0300

    First Commit

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace programaC
             float numero1 = 1;
             float numero2 = 2;
             float soma = numero1 + numero2;
+            
             Console.WriteLine(soma.ToString());
         }
     }
